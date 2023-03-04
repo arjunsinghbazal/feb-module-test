ad8ba39b67ea14928007865b2c014514269e2ab9
Arjun Singh <arjunsinghbazal@gmail.com>
2023-03-04 20:43:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var modal = document.getElementById('myModal');
+var btn =  document.getElementById('mybtn');
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
